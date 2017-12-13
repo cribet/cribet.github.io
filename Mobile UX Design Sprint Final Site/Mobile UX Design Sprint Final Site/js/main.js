22626ee4f35f01d8491da55e4271ad398fa1d7a9
cribet <32885458+cribet@users.noreply.github.com>
2017-12-13 11:20:43 -0600

    Add files via upload

---
+++ Mobile UX Design Sprint Final Site/Mobile UX Design Sprint Final Site/js/main.js
@@ -0,0 +1,9 @@
+// When the user clicks the hamburger menu
+  //hide the hamburger menu
+  //slideOut the #menu
+
+//When the user clicks the header
+  //slideOut the #location
+
+//When the user hovers over #location
+  //show image of location map
